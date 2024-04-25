af77ec1572b7160bf73c101c5740f0e9fd641032
Gramos <contact@gramosukaj.fr>
2024-04-25 16:48:56 +0200

    feat: always display Cast button

--- src/ts/components/casttogglebutton.ts
+++ src/ts/components/casttogglebutton.ts
@@ -7,42 +7,37 @@ import { i18n } from '../localization/i18n';
  * A button that toggles casting to a Cast receiver.
  */
 export class CastToggleButton extends ToggleButton<ToggleButtonConfig> {
-
   constructor(config: ToggleButtonConfig = {}) {
     super(config);
 
-    this.config = this.mergeConfig(config, {
-      cssClass: 'ui-casttogglebutton',
-      text: i18n.getLocalizer('googleCast'),
-    }, this.config);
+    this.config = this.mergeConfig(
+      config,
+      {
+        cssClass: 'ui-casttogglebutton',
+        text: i18n.getLocalizer('googleCast'),
+      },
+      this.config,
+    );
   }
 
   configure(player: PlayerAPI, uimanager: UIInstanceManager): void {
     super.configure(player, uimanager);
 
     this.onClick.subscribe(() => {
-      if (player.isCastAvailable()) {
-        if (player.isCasting()) {
-          player.castStop();
-        } else {
-          player.castVideo();
-        }
-      } else {
+      if (!player.isCastAvailable()) {
+        player.castVideo();
         if (console) {
           console.log('Cast unavailable');
         }
+        return;
+      }
+
+      if (player.isCasting()) {
+        player.castStop();
+      } else {
+        player.castVideo();
       }
     });
-
-    let castAvailableHander = () => {
-      if (player.isCastAvailable()) {
-        this.show();
-      } else {
-        this.hide();
-      }
-    };
-
-    player.on(player.exports.PlayerEvent.CastAvailable, castAvailableHander);
 
     // Toggle button 'on' state
     player.on(player.exports.PlayerEvent.CastWaitingForDevice, () => {
@@ -57,7 +52,6 @@ export class CastToggleButton extends ToggleButton<ToggleButtonConfig> {
     });
 
     // Startup init
-    castAvailableHander(); // Hide button if Cast not available
     if (player.isCasting()) {
       this.on();
     }
